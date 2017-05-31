e280ca18351277d239003d0b3a73c91b61fbc2a4
Alex Horton <alexhortonmusic@gmail.com>
2017-05-31 12:55:01 -0500

    front page matches ryans mock

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation();
+
+// --------- Homepage ----------- //
+
+// call to action btn in hero image
+let heroCTA = $('.hero-cta')
+
+// hover effects
+heroCTA.hover(
+  function() {
+    $('.hero-cta .btn').addClass( "btn-hover" );
+    $('.hero-cta .btn p').addClass( "btn-hover-p" );
+  },
+  function() {
+    $('.hero-cta .btn').removeClass( "btn-hover" );
+    $('.hero-cta .btn p').removeClass( "btn-hover-p" );
+  }
+)

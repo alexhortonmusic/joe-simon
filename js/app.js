3b5232b8edc8121069563208c57e9c9ed038e138
Alex Horton <alexhortonmusic@gmail.com>
2017-06-02 14:30:37 -0500

    scaffolding for homepage

--- js/app.js
+++ js/app.js
@@ -2,17 +2,64 @@ $(document).foundation();
 
 // --------- Homepage ----------- //
 
-// call to action btn in hero image
-let heroCTA = $('.hero-cta')
-
+// ---------- TRY TO FIGURE THIS OUT!!!!! ----------- //
 // hover effects
-heroCTA.hover(
+// function addHoverClass (button) {
+//   let test = button + ' .btn'
+//   let test2 = button + ' .btn p'
+//   $(test).addClass( "btn-hover" )
+//   $(test2).addClass( "btn-hover-p" )
+// }
+//
+// function removeHoverClass (button) {
+//   let test = button + ' .btn'
+//   let test2 = button + ' .btn p'
+//
+//   $(test).removeClass( "btn-hover" )
+//   $(test2).removeClass( "btn-hover-p" )
+// }
+
+// $(heroCTA).hover(
+//   addHoverClass(heroCTA),
+//   removeHoverClass(heroCTA)
+// )
+
+
+// call to action btn in hero image
+let hoverCTA = '.cta'
+
+$(hoverCTA).hover(
   function() {
-    $('.hero-cta .btn').addClass( "btn-hover" );
-    $('.hero-cta .btn p').addClass( "btn-hover-p" );
+    let btn = '#' + $(this)[0].id
+    $(btn + ' .btn').addClass( "btn-hover" )
+    $(btn + ' .btn p').addClass( "btn-hover-p" )
   },
   function() {
-    $('.hero-cta .btn').removeClass( "btn-hover" );
-    $('.hero-cta .btn p').removeClass( "btn-hover-p" );
+    let btn = '#' + $(this)[0].id
+    $(btn + ' .btn').removeClass( "btn-hover" )
+    $(btn + ' .btn p').removeClass( "btn-hover-p" )
   }
 )
+
+// ----------- show/hide "Joe's Story" on click - On homepage ----------- //
+
+$('.fullContent').readmore({
+  // collapsedHeight needs to be precise for the exact content
+  collapsedHeight: 110,
+  moreLink: "<p class='showMore'>Read whole story <i class='fa fa-caret-square-o-down' aria-hidden='true'></i></p>",
+  lessLink: `<p class='showLess'>See less <i class="fa fa-caret-square-o-up" aria-hidden="true"></i></p>`
+})
+
+// showMore.click(
+//   function() {
+//     showMore.addClass('hide')
+//     $('.fullContent').removeClass('hide')
+//   }
+// )
+//
+// showLess.click(
+//   function() {
+//     $('.fullContent').addClass('hide')
+//     showMore.removeClass('hide')
+//   }
+// )
